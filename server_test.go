eb129a33d3040d7c1b66e51674e63da535873751
msornay <msornay@gmail.com>
2016-05-16 21:21:11 +0200

    POST creates random ids

--- server_test.go
+++ server_test.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"testing"
+	"time"
 )
 
-var clientErrorTest = []struct {
+var statusesTest = []struct {
 	method string
 	path   string
 	code   int
@@ -15,10 +18,11 @@ var clientErrorTest = []struct {
 	{"HEAD", "/", 405},
 	{"PUT", "/", 405},
 	{"GET", "/XXX", 404},
+	{"POST", "/", 200},
 }
 
-func TestClientError(t *testing.T) {
-	for _, tt := range clientErrorTest {
+func TestStatuses(t *testing.T) {
+	for _, tt := range statusesTest {
 		r := &http.Request{
 			Method: tt.method,
 			URL: &url.URL{
@@ -32,4 +36,20 @@ func TestClientError(t *testing.T) {
 			t.Errorf("%s %s = %d want %d", tt.method, tt.path, rr.Code, tt.code)
 		}
 	}
+}
+
+
+func TestSample(t *testing.T) {
+	list := []string{"a", "b", "c", "d"}
+	s := sample(list, 2)
+	if len(s) != 2 {
+		t.Errorf("sample length : %s want %d", len(s), 2)
+	}
+}
+
+
+func TestMain(m *testing.M) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	retCode := m.Run()
+	os.Exit(retCode)
 }
